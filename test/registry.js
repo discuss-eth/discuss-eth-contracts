146308b101327d82b2d6281237354bf1a6d03995
moodysalem <a454465c8beef70811a01489042c79b7f235df5e>
2017-10-17 00:25:19 -0400

    more passing tests

--- test/registry.js
+++ test/registry.js
@@ -1,10 +1,11 @@
-import { expectError } from './utils';
+import promiseMe from 'mocha-promise-me';
 
 const Registry = artifacts.require('./Registry.sol');
 const User = artifacts.require('./User.sol');
+const Forum = artifacts.require('./Forum.sol');
 
 contract('Registry', async accounts => {
-  let registry, userAddress, forumAddress;
+  let registry, userAddress, forumAddress, user, forum;
   const [ registryOwner, forumOwner, userOwner ] = accounts;
 
   const USER_NAME = 'moodysalem';
@@ -14,21 +15,28 @@ contract('Registry', async accounts => {
     return await registry.hashName(name);
   }
 
-  beforeEach('create registry, forum and user', async () => {
+  beforeEach('create registry', async () => {
     registry = await Registry.new({ from: registryOwner });
+  });
 
+  beforeEach('create forum', async () => {
     const registerForumTx = await registry.registerForum(FORUM_NAME, 10, { from: forumOwner });
     forumAddress = registerForumTx.logs.find(log => log.event === 'LogRegisterForum').args.newForumAddress;
+    forum = Forum.at(forumAddress);
+  });
 
+  beforeEach('create user', async () => {
     const registerUserTx = await registry.registerUser(USER_NAME, { from: userOwner });
     userAddress = registerUserTx.logs.find(log => log.event === 'LogRegisterUser').args.newUserAddress;
+    user = User.at(userAddress);
   });
+
 
   describe('registerUser', async () => {
     it('prevents two users from registering with the same name', async () => {
       const DUPE_NAME = 'amfdlksmfeopwm';
       await registry.registerUser(DUPE_NAME, { from: accounts[ 0 ] });
-      await expectError(async () => await registry.registerUser(DUPE_NAME, { from: accounts[ 0 ] }));
+      promiseMe.thatYouReject(async () => await registry.registerUser(DUPE_NAME, { from: accounts[ 0 ] }));
     });
   });
 
@@ -43,7 +51,7 @@ contract('Registry', async accounts => {
     });
 
     it('returns zero address when user does not exist', async () => {
-      expectError(async () => {
+      promiseMe.thatYouReject(async () => {
         const hash = await hashName('name_does_not_exist');
 
         assert.strictEqual(
@@ -52,6 +60,25 @@ contract('Registry', async accounts => {
         );
       });
     });
+  });
+
+  describe('forum', async () => {
+    describe('setReputationThreshold', async () => {
+      it('cannot be called by the registry owner', async () => {
+        promiseMe.thatYouReject(async () => await forum.setReputationThreshold(20, { from: registryOwner }));
+      });
+
+      it('cannot be called by a user', async () => {
+        promiseMe.thatYouReject(async () => await forum.setReputationThreshold(20, { from: userOwner }));
+      });
+
+      it('can be called by the forum owner', async () => {
+        await forum.setReputationThreshold(20, { from: forumOwner });
+        const reputationThreshold = await forum.reputationThreshold();
+        assert.strictEqual(reputationThreshold.valueOf(), '20');
+      });
+    });
+
   });
 
 });
--- test/utils.js
+++
@@ -1,9 +0,0 @@
-export async function expectError(func) {
-  let didError = false;
-  try {
-    await func();
-  } catch (error) {
-    didError = true;
-  }
-  assert.strictEqual(didError, true);
-}
