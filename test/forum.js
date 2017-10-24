8f4456e217c4b1beeb2ca1bc021e782ead038960
moodysalem <a454465c8beef70811a01489042c79b7f235df5e>
2017-10-23 21:37:07 -0400

    tests back to passing

--- test/forum.js
+++ test/forum.js
@@ -1,5 +1,7 @@
 import promiseMe from 'mocha-promise-me';
-import { ZERO_ADDRESS, Registry, Forum } from './util/constants';
+import { Registry, Forum } from './util/constants';
+
+const POST_SUBJECT = 'a great discussion';
 
 contract('Forum', async ([ registryOwner, forumOwner, userOneOwner, nobody ]) => {
   let registry, forumAddress, forum, userOneNameHash, exampleContentHash;
@@ -57,40 +59,47 @@ contract('Forum', async ([ registryOwner, forumOwner, userOneOwner, nobody ]) =>
    event LogPost(bytes32 indexed userNameHash, bytes32 indexed threadKey, uint inReplyTo, address sender, uint newPostId);
    */
   describe('#post', async () => {
-    //it('requires a registered user', async () => {
-    //  promiseMe.thatYouReject(forum.post(0, userOneNameHash, 'a great discussion', exampleContentHash, [], [], { from: userOneOwner }));
-    //});
+    it('requires a registered user', async () => {
+      promiseMe.thatYouReject(
+        forum.post(0, userOneNameHash, POST_SUBJECT, exampleContentHash, [], [], { from: nobody })
+      );
+    });
 
-    //it('requires a user that meets the threshold', async () => {
-    //  await forum.setReputationThreshold(10, { from: forumOwner });
-    //
-    //  promiseMe.thatYouReject(forum.post(0, userOneNameHash, 'a great discussion', exampleContentHash, [], [], { from: userOneOwner }));
-    //});
-    //
+    it('requires a user that meets the threshold', async () => {
+      await forum.setReputationThreshold(10, { from: forumOwner });
+
+      promiseMe.thatYouReject(
+        forum.post(0, userOneNameHash, POST_SUBJECT, exampleContentHash, [], [], { from: userOneOwner })
+      );
+    });
+
     it('allows post if i meet threshold', async () => {
-      const result = await forum.post(0, userOneNameHash, 'a great discussion', exampleContentHash, [], [], { from: userOneOwner });
+      const result = await forum.post(0, userOneNameHash, POST_SUBJECT, exampleContentHash, [], [], { from: userOneOwner });
       const { logs: [ log ] } = result;
       assert.strictEqual(log.event, 'LogPost');
       assert.strictEqual(log.args.userNameHash, userOneNameHash);
-      const subjectHash = await hashName('a great discussion');
+      const subjectHash = await hashName(POST_SUBJECT);
       assert.strictEqual(log.args.threadKey, subjectHash);
       assert.strictEqual(log.args.inReplyTo.valueOf(), '0');
       assert.strictEqual(log.args.sender, userOneOwner);
       assert.strictEqual(log.args.newPostId.valueOf(), '1');
     });
-    //
-    //it('allows post if i exceed threshold', async () => {
-    //  await forum.setReputationThreshold(-10, { from: forumOwner });
-    //
-    //  await forum.post(0, userOneNameHash, 'a great discussion', [], [], { from: userOneOwner });
-    //});
-    //
-    //it('fires an event', async () => {
-    //  const { logs } = await forum.post(userOneNameHash, 'a great discussion', [], [], { from: userOneOwner });
-    //  assert.strictEqual(logs[ 0 ].event, 'LogPost');
-    //  assert.strictEqual(logs[ 0 ].args.sender, userOneOwner);
-    //  assert.strictEqual(logs[ 0 ].args.threadNameHash, await hashName('a great discussion'));
-    //});
+
+    it('allows post if i exceed threshold', async () => {
+      await forum.setReputationThreshold(-10, { from: forumOwner });
+
+      await forum.post(0, userOneNameHash, POST_SUBJECT, exampleContentHash, [], [], { from: userOneOwner });
+    });
+
+    it('fires an event', async () => {
+      const { logs } =
+        await forum.post(0, userOneNameHash, POST_SUBJECT, exampleContentHash, [], [], { from: userOneOwner });
+
+      const subjectHash = await hashName(POST_SUBJECT);
+      assert.strictEqual(logs[ 0 ].event, 'LogPost');
+      assert.strictEqual(logs[ 0 ].args.sender, userOneOwner);
+      assert.strictEqual(logs[ 0 ].args.threadKey, subjectHash);
+    });
   });
 
 });
