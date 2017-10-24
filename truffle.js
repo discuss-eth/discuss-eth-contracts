332320f6034a3888ce1695a6ec7a07213e244fbb
moodysalem <a454465c8beef70811a01489042c79b7f235df5e>
2017-10-23 22:09:43 -0400

    support all the networks via infura

--- truffle.js
+++ truffle.js
@@ -1,6 +1,23 @@
 require('babel-register');
 require('babel-polyfill');
 
+const _ = require('underscore');
+const { INFURA_API_KEY, MNEMONIC } = process.env;
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+const NETWORK_IDS = {
+  mainnet: 1,
+  ropsten: 2,
+  rinkeby: 4,
+  kovan: 42
+};
+
 module.exports = {
-  networks: {}
+  networks: _.mapObject(
+    NETWORK_IDS,
+    (network_id, name) => ({
+      provider: new HDWalletProvider(MNEMONIC, 'https://' + name + '.infura.io/' + INFURA_API_KEY),
+      network_id
+    })
+  )
 };
